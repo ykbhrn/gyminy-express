a2abb2b1e1cbb8e3c80fce7c032d00a18a23c35a
ykbhrn <jakub.horun@gmail.com>
2021-01-03 01:50:07 +0100

    db

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 8000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/gyminy-express-db'
+const dbURI = 'mongodb+srv://jakub:61221698Kubo@cluster0.cxpcf.mongodb.net/gyminy-db?retryWrites=true&w=majority'
 
 module.exports = { port, dbURI }
